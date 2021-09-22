d02bb2e946eb6d9c9c1664b7f5fb7bc3fd0d2795
ateeh <ateeh42@gmail.com>
2021-09-22 10:16:42 +0200

    title screen created

---
+++ RacingDemo/Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuUIHandler : MonoBehaviour
+{
+  public void StartNew()
+  {
+    SceneManager.LoadScene(1);
+  }
+
+  public void Exit()
+  {
+#if UNITY_EDITOR
+    EditorApplication.ExitPlaymode();
+#else
+    Application.Quit(); // original code to quit Unity player
+#endif
+  }
+}
