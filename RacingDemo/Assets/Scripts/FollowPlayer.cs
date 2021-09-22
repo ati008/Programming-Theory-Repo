1e9dde6e9c2998c1f742d7d129020d304c9932a8
ateeh <ateeh42@gmail.com>
2021-09-22 10:27:28 +0200

    imported assets

---
+++ RacingDemo/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+  public GameObject player;
+  [SerializeField] Vector3 offset = new Vector3(30, 0, 10);
+  Vector3 rotation = new Vector3(0, -90, 0);
+  // Start is called before the first frame update
+  void Start()
+  {
+    transform.Rotate(player.transform.rotation.eulerAngles+rotation);
+  }
+
+  // Update is called once per frame
+  void LateUpdate()
+  {
+    transform.position = player.transform.position + offset;
+  }
+}
---
+++ RacingDemo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  [SerializeField] float speed = 20.0f;
+  [SerializeField] float turnSpeed=45.0f;
+  float horizontalInput;
+  float forwardInput;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    horizontalInput = Input.GetAxis("Horizontal");
+    forwardInput = Input.GetAxis("Vertical");
+    // Move the vehicle forward
+    transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+    transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+  }
+}
